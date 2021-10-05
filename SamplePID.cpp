c6f81228bcd7933207e0cbc11707c6e5ae4bc1a9
Rzheng1324 <91423368+Rzheng1324@users.noreply.github.com>
2021-10-05 15:48:58 -0400

    Update SamplePID.cpp

--- SamplePID.cpp
+++ SamplePID.cpp
@@ -1,1 +1,85 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       ryanzheng                                                 */
+/*    Created:      Tue Sep 28 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// RightMotor           motor         1               
+// LeftMotor            motor         9               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include <cmath>
+using namespace vex;
+// Field size is 12 by 12 feet
+private static final int fieldSpecs = 12;
+competition Competition;
+void pre_auton(void){
+  vexcodeInit();
+  
+}
+double kP = 0.0;
+double kI = 0.0;
+double kD = 0.0;
+int error;
+int prevError;
+
+bool PIDstart = false;
+int drivePID{
+  
+  
+  while (PIDstart){
+    
+    wait(20, msec);
+  }
+  return 1;
+}
+void autonomous(void){
+  vex::task pid(drivePID);
+  PIDstart = true;
+}
+
+
+
+
+
+
+
+
+
+void usercontrol(void){
+  double turnImportance = 0.5;
+  while (1){
+
+    double turnVal = Controller1.Axis1.position(percent);
+    double fowardVal = Controller1.Axis3.position(percent);
+
+    double turnVolts = turnVal * 0.12;
+    double fowardVolts = fowardVal * 0.12 * (1 - (std::abs(turnVolts)/12.0)) * turnImportance;
+
+    LeftMotor.spin(forward, fowardVolts - turnVolts, voltageUnits::volt);
+    RightMotor.spin(forward, fowardVolts + turnVolts, voltageUnits::volt);
+
+
+    wait(20, msec);
+  }
+}
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  pre_auton();
+
+  while(true){
+    wait(100, msec);
+  }
+  
+}
